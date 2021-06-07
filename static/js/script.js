7bad7ea07b4229c875e4be5fc1160ebb6e850f3b
andrewboyd79 <andrewboyd79@googlemail.com>
2021-06-07 20:43:10 +0000

    Amend styling to Search section

--- static/js/script.js
+++ static/js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 Code taken & amended from stackoverflow post/solution 
 (https://stackoverflow.com/questions/895171/prevent-users-from-submitting-a-form-by-hitting-enter)
 */
-$('#form-details').on("keydown", "form", function(event) { 
+$(document).on("keydown", "form", function(event) { 
     return event.key != "Enter";
 })
 
@@ -43,3 +43,44 @@ $('.method-delete').on('click', function(){
     $(this).next().remove();
     $(this).remove()
 });
+
+  validateMaterializeSelect();
+
+  function validateMaterializeSelect() {
+      let classValid = {
+          "border-bottom": "1px solid #4caf50",
+          "box-shadow": "0 1px 0 0 #4caf50"
+      };
+      let classInvalid = {
+          "border-bottom": "1px solid #f44336",
+          "box-shadow": "0 1px 0 0 #f44336"
+      };
+      if ($("select.validate").prop("required")) {
+          $("select.validate").css({
+              "display": "block",
+              "height": "0",
+              "padding": "0",
+              "width": "0",
+              "position": "absolute"
+          });
+      }
+      $(".select-wrapper input.select-dropdown").on("focusin", function () {
+          $(this).parent(".select-wrapper").on("change", function () {
+              if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () {})) {
+                  $(this).children("input").css(classValid);
+              }
+          });
+      }).on("click", function () {
+          if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+              $(this).parent(".select-wrapper").children("input").css(classValid);
+          } else {
+              $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                  if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                      if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                          $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                      }
+                  }
+              });
+          }
+      });
+  };
